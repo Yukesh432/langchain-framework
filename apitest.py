a02884844e93bbd80a56b0bd6f644917f0256440
Yukesh432 <nsiwansh@gmail.com>
2023-11-05 00:09:31 +0545

    working

--- apitest.py
+++ apitest.py
@@ -4,18 +4,37 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 import os
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
 
-OPENAI_API_KEY= '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'
+
+
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-template = """Question: {question}
+# template = """Question: {question}
 
-Answer: Let's think step by step."""
+# Answer: Let's think step by step."""
 
-prompt = PromptTemplate(template=template, input_variables=["question"])
+# prompt = PromptTemplate(template=template, input_variables=["question"])
 
-llm= OpenAI()
-llm_chain = LLMChain(prompt=prompt, llm=llm)
-question = "What NFL team won the Super Bowl in the year Justin Beiber was born?"
+# llm= OpenAI()
+# llm_chain = LLMChain(prompt=prompt, llm=llm)
+# question = "What is deep learning ? How it is different from machine learning?"
 
-print(llm_chain.run(question))
+# print(llm_chain.run(question))
+chat = ChatOpenAI(temperature=0)
+messages = [
+    SystemMessage(
+        content="You are a helpful assistant that translates English to French."
+    ),
+    HumanMessage(
+        content="Translate this sentence from English to French. I love programming."
+    ),
+]
+print(chat(messages))
