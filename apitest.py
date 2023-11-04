d3dd63343c0405f6e00c0cd9c4b13ab9f358eadd
Yukesh432 <nsiwansh@gmail.com>
2023-11-04 23:57:22 +0545

    working

--- apitest.py
+++ apitest.py
@@ -1,1 +1,21 @@
 import openai
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+import os
+
+OPENAI_API_KEY= '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'
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+llm= OpenAI()
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+question = "What NFL team won the Super Bowl in the year Justin Beiber was born?"
+
+print(llm_chain.run(question))
