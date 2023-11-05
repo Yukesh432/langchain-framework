5dd336cb0e361b60827c00bd668383df300dc0fe
Yukesh432 <nsiwansh@gmail.com>
2023-11-05 20:24:42 +0545

    temperature change

--- proxyLLM/convagent.py
+++ proxyLLM/convagent.py
@@ -7,7 +7,7 @@ from langchain.llms import OpenAI
 # tool= ShellTool()
 # print(tool.run("echo this is working"))
 
-llm = OpenAI(temperature=0)
+llm = OpenAI(temperature=0.5)
 tools = load_tools(["wikipedia", "llm-math", "terminal"], llm=llm)
 agent = initialize_agent(
     tools,
